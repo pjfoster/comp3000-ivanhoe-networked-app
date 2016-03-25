a6dc6d9d73c5c2e98297afcaec3b569b24c169ee
Patricia Foster <PJF@Patricias-MacBook-Pro.local>
2016-03-25 13:09:25 -0400

    Additional tests for action cards

--- test/comp3004/invahoe/action_cards/ModifyDisplayTest.java
+++ test/comp3004/invahoe/action_cards/ModifyDisplayTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 
 import comp3004.ivanhoe.controller.MockController;
 import comp3004.ivanhoe.model.ActionCard;
+import comp3004.ivanhoe.model.Card;
 import comp3004.ivanhoe.model.ColourCard;
 import comp3004.ivanhoe.model.Player;
 import comp3004.ivanhoe.model.SupporterCard;
@@ -21,6 +22,9 @@ import comp3004.ivanhoe.server.MockServer;
 import comp3004.ivanhoe.util.ClientRequestBuilder;
 import comp3004.ivanhoe.util.ServerResponseBuilder;
 
+// TODO: test with opponents who have withdrawn, test with card not in hand
+// TODO: test with single card in opponent display
+
 public class ModifyDisplayTest {
 
 	HashMap<Integer, Player> players;
@@ -28,7 +32,7 @@ public class ModifyDisplayTest {
 	ClientRequestBuilder requestBuilder = new ClientRequestBuilder();
 	Tournament tournament;
 	MockController controller;
-	Player alexei, luke;
+	Player alexei, luke, jayson, emma;
 	ArrayList<String> cardWrapper;
 	
 	@Before
@@ -36,10 +40,14 @@ public class ModifyDisplayTest {
 		
 		alexei = new Player("Alexei");
 		luke = new Player("Luke");
+		emma = new Player("Emma");
+		jayson = new Player("Jayson");
 		
 		players = new HashMap<Integer, Player>();
 		players.put(60001, alexei);
 		players.put(60002, luke);
+		players.put(60003, jayson);
+		players.put(60004, emma);
 		
 		controller = new MockController(new MockServer(), responseBuilder, 2);
 		controller.setPlayers(players);
@@ -89,11 +97,6 @@ public class ModifyDisplayTest {
 	}
 	
 	@Test
-	public void testBreakLanceInvalid() {
-
-	}
-	
-	@Test
 	public void testRiposte() {
 
 		ActionCard riposte = (ActionCard)controller.getCardFromDeck("riposte");
@@ -123,11 +126,6 @@ public class ModifyDisplayTest {
 		assertEquals(alexei.getDisplay().size(), 1);
 		assertEquals(alexei.getDisplay().get(0).toString(), "s3");
 		
-	}
-	
-	@Test
-	public void testRiposteInvalid() {
-
 	}
 	
 	@Test
@@ -171,7 +169,23 @@ public class ModifyDisplayTest {
 	
 	@Test
 	public void testDodgeInvalid() {
-
+		
+		// card isn't in opponent's display
+		ActionCard dodge = (ActionCard)controller.getCardFromDeck("dodge");
+		alexei.addHandCard(dodge);
+		cardWrapper.add("dodge");
+		assertTrue(controller.playCard(cardWrapper));
+		
+		JSONObject selectOpponent = requestBuilder.buildSelectOpponent("luke");
+		controller.processPlayerMove(60001, selectOpponent);
+		
+		JSONObject pickCard = requestBuilder.buildPickCard("b5");
+		controller.processPlayerMove(60001, pickCard);
+		
+		// check that it is still Alexei's turn - the move wasn't processed
+		assertEquals(controller.getCurrentTurnPlayer(), alexei);
+		assertEquals(controller.getState(), 8);
+		assertEquals(tournament.getDiscardPile().size(), 0);
 	}
 	
 	@Test
@@ -213,7 +227,138 @@ public class ModifyDisplayTest {
 	
 	@Test
 	public void testRetreatInvalid() {
+		// card isn't in opponent's display
+		ActionCard retreat = (ActionCard)controller.getCardFromDeck("retreat");
+		alexei.addHandCard(retreat);
+		cardWrapper.add("retreat");
+		assertTrue(controller.playCard(cardWrapper));
 
+		JSONObject selectOpponent = requestBuilder.buildSelectOpponent("luke");
+		controller.processPlayerMove(60001, selectOpponent);
+
+		JSONObject pickCard = requestBuilder.buildPickCard("b5");
+		controller.processPlayerMove(60001, pickCard);
+
+		// check that it is still Alexei's turn - the move wasn't processed
+		assertEquals(controller.getCurrentTurnPlayer(), alexei);
+		assertEquals(controller.getState(), 8);
+		assertEquals(tournament.getDiscardPile().size(), 0);
+	}
+	
+	@Test
+	public void testKnockDown() {
+		ActionCard knockDown = (ActionCard)controller.getCardFromDeck("knockdown");
+		alexei.addHandCard(knockDown);
+		cardWrapper.add("knockdown");
+		
+		luke.addHandCard(new ColourCard("blue", 3));
+		luke.addHandCard(new ColourCard("blue", 5));
+		luke.addHandCard(new SupporterCard("squire", 2));
+		
+		assertTrue(controller.playCard(cardWrapper));
+		
+		// check that Alexei is now required to pick her opponent
+		assertEquals(controller.getCurrentTurnPlayer(), alexei);
+		assertEquals(controller.getState(), 7);
+		
+		JSONObject selectOpponent = requestBuilder.buildSelectOpponent("luke");
+		controller.processPlayerMove(60001, selectOpponent);
+		
+		// check that Alexei's hand now contains one of Luke's cards
+		assertEquals(luke.getHand().size(), 2);
+		assertEquals(alexei.getHand().size(), 1);
+		assertTrue(alexei.getHand().get(0).toString() == "b5" ||
+				   alexei.getHand().get(0).toString() == "b3" ||
+				   alexei.getHand().get(0).toString() == "s3");
+	}
+	
+	@Test
+	public void testOutmaneuver() {
+		ActionCard outmaneuver = (ActionCard)controller.getCardFromDeck("outmaneuver");
+		alexei.addHandCard(outmaneuver);
+		cardWrapper.add("outmaneuver");
+		
+		luke.addDisplayCard(new ColourCard("blue", 5));
+		luke.addDisplayCard(new ColourCard("blue", 3));
+		
+		jayson.addDisplayCard(new SupporterCard("squire", 2));
+		jayson.addDisplayCard(new SupporterCard("maiden", 6));
+		jayson.addDisplayCard(new ColourCard("blue", 3));
+		
+		emma.addDisplayCard(new ColourCard("blue", 4));
+
+		assertTrue(controller.playCard(cardWrapper));
+		
+		// check that the displays were modified, except for Emma's
+		assertEquals(luke.getDisplay().size(), 1);
+		assertTrue(luke.getDisplay().get(0).toString() != "b5");
+		assertEquals(jayson.getDisplay().size(), 2);
+		assertTrue(jayson.getDisplay().get(1).toString() != "s3");
+		assertEquals(emma.getDisplay().size(), 1);
+	}
+	
+	@Test
+	public void testCharge() {
+		// TODO: would it also be removed from Lexi's hand?
+		
+		ActionCard charge = (ActionCard)controller.getCardFromDeck("charge");
+		alexei.addHandCard(charge);
+		cardWrapper.add("charge");
+		
+		luke.addDisplayCard(new ColourCard("blue", 5));
+		luke.addDisplayCard(new ColourCard("blue", 2));
+		luke.addDisplayCard(new ColourCard("blue", 3));
+		
+		jayson.addDisplayCard(new ColourCard("blue", 3));
+		jayson.addDisplayCard(new ColourCard("blue", 2));
+		
+		emma.addDisplayCard(new ColourCard("blue", 2));
+
+		assertTrue(controller.playCard(cardWrapper));
+		
+		// check that the displays were modified, except for Emma's
+		assertEquals(luke.getDisplay().size(), 2);
+		assertEquals(jayson.getDisplay().size(), 1);
+		assertEquals(emma.getDisplay().size(), 1);
+		
+		// check that no one's hand contains b2
+		for (Player p: tournament.getPlayers().values()) {
+			for (Card c: p.getHand()) {
+				assertTrue(c.toString() != "b2");
+			}
+		}
+	}
+	
+	@Test
+	public void testCounterCharge() {
+		// TODO: would it also be removed from Lexi's hand?
+		
+				ActionCard charge = (ActionCard)controller.getCardFromDeck("charge");
+				alexei.addHandCard(charge);
+				cardWrapper.add("charge");
+				
+				luke.addDisplayCard(new ColourCard("blue", 5));
+				luke.addDisplayCard(new ColourCard("blue", 2));
+				luke.addDisplayCard(new ColourCard("blue", 3));
+				
+				jayson.addDisplayCard(new ColourCard("blue", 5));
+				jayson.addDisplayCard(new ColourCard("blue", 2));
+				
+				emma.addDisplayCard(new ColourCard("blue", 5));
+
+				assertTrue(controller.playCard(cardWrapper));
+				
+				// check that the displays were modified, except for Emma's
+				assertEquals(luke.getDisplay().size(), 2);
+				assertEquals(jayson.getDisplay().size(), 1);
+				assertEquals(emma.getDisplay().size(), 1);
+				
+				// check that no one's hand contains b2
+				for (Player p: tournament.getPlayers().values()) {
+					for (Card c: p.getHand()) {
+						assertTrue(c.toString() != "b5");
+					}
+				}
 	}
 
 }
